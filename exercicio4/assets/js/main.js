6d017aff4495d83afd3f72ad7451cc7836f9ecb2
lucasgdo <lucasgdo4@gmail.com>
2020-03-15 10:52:29 -0300

    exercicio 4

---
+++ exercicio4/assets/js/main.js
@@ -0,0 +1,27 @@
+function main() {
+    const texto = document.querySelector('.container');
+    const elementos = [
+        {tag: 'p', texto: 'Frase 1'},
+        {tag: 'div', texto: 'Frase 2'},
+        {tag : 'footer', texto: 'Frase 3'},
+        {tag: 'section', texto: 'Frase 4'}
+    ];
+
+    const div = criaDiv();
+    for(i=0; i<elementos.length ; i++) {
+        let elemento = elementos[i].tag;
+        let textoElemento = elementos[i].texto;
+        let elementoCriado = document.createElement(elemento);
+        elementoCriado.innerHTML = textoElemento;
+        div.appendChild(elementoCriado);
+    }
+
+    texto.appendChild(div);
+
+    function criaDiv() {
+        const div = document.createElement('div');
+        return div;
+    }
+}
+
+main();
